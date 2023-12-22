654efab5ff4294094b6a6cc32162aa929543ba3c
Olivi3rCar <carglassolivier2006@gmail.com>
2023-12-22 14:14:41 +0100

    oui voilà

--- Snake/snake.py
+++ Snake/snake.py
@@ -3,7 +3,7 @@ from tkinter import *
 class Cellule :
     def __init__(self, x, y):
         self.valeur = 0 # Cellule vide au départ
-        self.pos = (x, y) # Position de la 
+        self.pos = (x, y) # Position de la cellule en fonction de x et y
 
 fenetre=Tk()
 fenetre.geometry("1200x800")
