bebb4dd5ab4582430d18301f73263e3a57fd3547
soupe-aux-oignons <may.bouton@gmail.com>
2024-02-09 11:23:01 +0100

    suelette du visualizer, reste draw et update à coder avnt de faire les premiers tests

--- ABR tree vis/BinarySearchTreeVisualizer.py
+++ ABR tree vis/BinarySearchTreeVisualizer.py
@@ -1,3 +1,27 @@
+from tkinter import *
+from BinarySearchTree import *
+
 class BinarySearchTreeVisualizer :
     def __init__(self):
+        window=Tk()
+        window.geometry("800x600")
+        window.configure(bg="white")
+        window.resizable(0,0)
+        self.window=window
+        
+        canevas=Canvas()
+        canevas.place(x=0,y=120,width=800,height=400)
+        canevas.configure(bg="white",bd=0)
+        self.canevas=canevas
+        self.tree=BinarySearchTree()
+        
+        
+        self.window.mainloop()
+        
+    def draw(self):
         pass
+    
+    def update(self):
+        pass
+        
+eh=BinarySearchTreeVisualizer()
