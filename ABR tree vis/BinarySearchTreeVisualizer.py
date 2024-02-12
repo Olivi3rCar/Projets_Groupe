2b90f56298dd639df986b21b5d055f31a0e14cb8
JesskaSteff <jessikamorisseau707@gmail.com>
2024-02-12 20:00:49 +0100

    draw

--- ABR tree vis/BinarySearchTreeVisualizer.py
+++ ABR tree vis/BinarySearchTreeVisualizer.py
@@ -18,10 +18,25 @@ class BinarySearchTreeVisualizer :
         
         self.window.mainloop()
         
-    def draw(self):
+    def draw(self, tree, x, y, depth=1):
+        self.tree = tree
+        self.coordonnee_centre = x, Y
+        self.centre_x =x 
+        self.centre_y = y
+        self.branche= canevas.create_line(self.centre_x, self.centre_y, self.fils_gauche_x, self.fils_droite_y)
+        self.noeuds =canevas.create_oval((self.centre_x - self.noeuds,
+                                          self.centre_y - self.noeuds, 
+                                          self.centre_x + self.noeuds,
+                                          self.centre_y + self.noeuds), fill ="gray")
+        self.value = canevas.create_text(self.centre_x, self.centre_y, tex(tree.value))
+        if fils_gauche != None:
         pass
+        if fils_droite != None:
+            pass
     
     def update(self):
-        pass
+        self.canevas.delete()
+        self.draw()
+        self.info_tab.update()
         
 eh=BinarySearchTreeVisualizer()
